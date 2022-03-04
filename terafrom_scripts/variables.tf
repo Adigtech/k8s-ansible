5227ada35292ed0eb4bcbef44db01b77e7ad2334
Kidunnu <78957600+Kidunnu@users.noreply.github.com>
2022-03-04 03:19:30 -0500

    Update variables.tf

--- terafrom_scripts/variables.tf
+++ terafrom_scripts/variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
-  default = "us-west-2"
+  default = "us-west-1"
 }
 
 variable "key_name" {
@@ -14,7 +14,7 @@ variable "subnets_cidr" {
 }
 variable "availability_zones" {
   type    = list(string)
-  default = ["us-west-2a", "us-west-2b"]
+  default = ["us-west-1a", "us-west-1b"]
 }
 variable "kubernetes_ami" {
   default = "ami-090717c950a5c34d3"
